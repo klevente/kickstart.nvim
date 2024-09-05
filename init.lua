9f7151db635e379114c27bbf71582e80850935b8
Levente Krizsán <lewalevi@gmail.com>
2024-09-05 22:28:00 +0200

    Add rustacean nvim

--- init.lua
+++ init.lua
@@ -622,7 +622,7 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         -- pyright = {},
-        rust_analyzer = {},
+        -- rust_analyzer = {},
         eslint = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
